38c749090c2efe27fb52e7d3785eeedf4187b5cd
sllorennte <sergiollp01@gmail.com>
2025-12-10 17:41:24 +0100

    mensajes: soporte primero

--- src/rutas/mensajeRutas.js
+++ src/rutas/mensajeRutas.js
@@ -4,6 +4,8 @@ const ctrl          = require('../controladores/mensajeController');
 const { verifyToken } = require('../middlewares/auth');
 
 router.get('/mensajes', verifyToken, ctrl.listarMensajes);
+// Colocar la ruta de soporte antes de la ruta con :id para evitar colisiones
+router.post('/mensajes/soporte', verifyToken, ctrl.enviarSoporte);
 router.post('/mensajes/:id', verifyToken, ctrl.responderMensaje);
 router.post('/mensajes', verifyToken, ctrl.crearMensaje);
 router.delete('/mensajes/:id', verifyToken, ctrl.eliminarMensaje);
